2b37e9e3e8aca701047bb8157ce8ae6d15d254db
Sean Matheson <sean@ctrlplusb.com>
2018-06-21 00:24:34 +0100

    Fixed plugins.

--- packages/plugin-build-babel/src/index.js
+++ packages/plugin-build-babel/src/index.js
@@ -35,91 +35,89 @@ type SanitisedOptions = {
   config: Object,
 }
 
-// :: Package, Options -> DevelopAPI
-module.exports = function babelBuildPlugin(
-  pkg: Package,
-  options: Options = {},
-): BuildPlugin {
-  const sanitiseOptions = (options: Options): SanitisedOptions => {
-    const { config, inputs } = options
+const babelBuildPlugin: BuildPlugin = {
+  name: '@lerna-cola/plugin-build-babel',
+  build: (pkg: Package, options: Options) => {
+    const sanitiseOptions = (opts: Options): SanitisedOptions => {
+      const { config, inputs } = opts
 
-    if (!config || typeof config !== 'string' || typeof config === 'object') {
-      TerminalUtils.errorPkg(
-        pkg,
-        'A babel config package name or object must be provided as options',
-      )
-      process.exit(1)
-    }
-
-    const resolveConfig = (): Object => {
-      const module = FsUtils.resolvePackage(config)
-      if (typeof module !== 'function' || typeof module !== 'object') {
+      if (!config || typeof config !== 'string' || typeof config === 'object') {
         TerminalUtils.errorPkg(
           pkg,
-          `The babel config ${config} is an invalid package. Should export an object or a funciton.`,
+          'A babel config package name or object must be provided as options',
         )
         process.exit(1)
+        throw new Error('💩')
       }
-      return typeof module === 'function' ? module(pkg, options) : module
-    }
 
-    return {
-      config: typeof config === 'string' ? resolveConfig() : config,
-      inputs: inputs || ['**/*.js', '**/*.jsx', '!__tests__', '!test.js'],
-    }
-  }
-
-  const sanitisedOptions = sanitiseOptions(options)
-
-  const patterns = sanitisedOptions.inputs.concat([
-    '!node_modules/**/*',
-    `!${path.basename(pkg.paths.packageBuildOutput)}/**/*`,
-  ])
-
-  // :: string -> Array<string>
-  const getJsFilePaths = () =>
-    globby(patterns, {
-      cwd: pkg.paths.packageSrc,
-    })
-
-  return {
-    name: '@lerna-cola/plugin-build-babel',
-    build: () =>
-      getJsFilePaths().then(filePaths => {
-        // :: string -> Promise<void>
-        const transpileFile = filePath => {
-          const writeTranspiledFile = result => {
-            const outFile = path.resolve(pkg.paths.packageBuildOutput, filePath)
-            ensureParentDirectoryExists(outFile)
-            fs.writeFileSync(outFile, result.code, { encoding: 'utf8' })
-            fs.writeFileSync(`${outFile}.map`, JSON.stringify(result.map), {
-              encoding: 'utf8',
-            })
-          }
-          const module = path.resolve(pkg.paths.packageSrc, filePath)
-          return transformFile(module, sanitisedOptions.config).then(
-            writeTranspiledFile,
+      const resolveConfig = (packageName): Object => {
+        const module = FsUtils.resolvePackage(packageName)
+        if (typeof module !== 'function' || typeof module !== 'object') {
+          TerminalUtils.errorPkg(
+            pkg,
+            `The babel config "${packageName}" is an invalid package. Should export an object or a funciton.`,
           )
+          process.exit(1)
         }
+        // $FlowFixMe
+        return typeof module === 'function' ? module(pkg, options) : module
+      }
 
-        const limit = pLimit(maxConcurrentTranspiles)
-        const queueTranspile = filePath => limit(() => transpileFile(filePath))
-        return Promise.all(R.map(queueTranspile, filePaths))
-      }),
-    clean: () =>
-      new Promise(resolve => {
-        if (fs.pathExistsSync(pkg.paths.packageBuildOutput)) {
-          fs.removeSync(pkg.paths.packageBuildOutput)
+      return {
+        config: typeof config === 'string' ? resolveConfig(config) : config,
+        inputs: inputs || ['**/*.js', '**/*.jsx', '!__tests__', '!test.js'],
+      }
+    }
+
+    const sanitisedOptions = sanitiseOptions(options)
+
+    const patterns = sanitisedOptions.inputs.concat([
+      '!node_modules/**/*',
+      `!${path.basename(pkg.paths.packageBuildOutput)}/**/*`,
+    ])
+
+    // :: string -> Array<string>
+    const getJsFilePaths = () =>
+      globby(patterns, {
+        cwd: pkg.paths.packageSrc,
+      })
+
+    return getJsFilePaths().then(filePaths => {
+      const transpileFile = filePath => {
+        const writeTranspiledFile = result => {
+          const outFile = path.resolve(pkg.paths.packageBuildOutput, filePath)
+          ensureParentDirectoryExists(outFile)
+          fs.writeFileSync(outFile, result.code, { encoding: 'utf8' })
+          fs.writeFileSync(`${outFile}.map`, JSON.stringify(result.map), {
+            encoding: 'utf8',
+          })
         }
-        resolve()
-      }),
-    deploy: () => {
-      TerminalUtils.errorPkg(pkg, '"deploy" not supported by "babel" plugin')
-      process.exit(1)
-    },
-    develop: () => {
-      TerminalUtils.errorPkg(pkg, '"develop" not supported by "babel" plugin')
-      process.exit(1)
-    },
-  }
+        const module = path.resolve(pkg.paths.packageSrc, filePath)
+        return transformFile(module, sanitisedOptions.config).then(
+          writeTranspiledFile,
+        )
+      }
+
+      const limit = pLimit(maxConcurrentTranspiles)
+      const queueTranspile = filePath => limit(() => transpileFile(filePath))
+      return Promise.all(R.map(queueTranspile, filePaths))
+    })
+  },
+  clean: (pkg: Package) =>
+    new Promise(resolve => {
+      if (fs.pathExistsSync(pkg.paths.packageBuildOutput)) {
+        fs.removeSync(pkg.paths.packageBuildOutput)
+      }
+      resolve()
+    }),
+  deploy: (pkg: Package) => {
+    TerminalUtils.errorPkg(pkg, '"deploy" not supported by "babel" plugin')
+    process.exit(1)
+  },
+  develop: (pkg: Package) => {
+    TerminalUtils.errorPkg(pkg, '"develop" not supported by "babel" plugin')
+    process.exit(1)
+  },
 }
+
+module.exports = babelBuildPlugin
--- packages/plugin-build-flow/src/index.js
+++ packages/plugin-build-flow/src/index.js
@@ -23,56 +23,53 @@ type PluginOptions = {
   inputs?: Array<string>,
 }
 
-// :: Package, Options -> DevelopAPI
-module.exports = function flowBuildPlugin(
-  pkg: Package,
-  options: PluginOptions,
-): BuildPlugin {
-  const patterns = (
-    options.inputs || ['**/*.js', '!__tests__', '!test.js']
-  ).concat(['!node_modules/**/*', `!${pkg.paths.packageBuildOutput}/**/*`])
+const flowBuildPlugin: BuildPlugin = {
+  name: '@lerna-cola/plugin-build-flow',
+  build: (pkg: Package, options: PluginOptions) => {
+    const patterns = (
+      options.inputs || ['**/*.js', '!__tests__', '!test.js']
+    ).concat(['!node_modules/**/*', `!${pkg.paths.packageBuildOutput}/**/*`])
 
-  // :: string -> Array<string>
-  const getJsFilePaths = () =>
-    globby(patterns, {
-      cwd: pkg.paths.packageSrc,
+    // :: string -> Array<string>
+    const getJsFilePaths = () =>
+      globby(patterns, {
+        cwd: pkg.paths.packageSrc,
+      })
+
+    return getJsFilePaths().then(filePaths => {
+      // :: string -> Promise<void>
+      const transpileFile = filePath =>
+        new Promise(resolve => {
+          const module = path.resolve(pkg.paths.packageSrc, filePath)
+          const input = fs.readFileSync(module, 'utf8')
+          const output = flowRemoveTypes(input)
+          const outFile = path.resolve(pkg.paths.packageBuildOutput, filePath)
+          ensureParentDirectoryExists(outFile)
+          fs.writeFileSync(outFile, output.toString(), { encoding: 'utf8' })
+          fs.writeFileSync(`${outFile}.flow`, input, { encoding: 'utf8' })
+          resolve()
+        })
+
+      const limit = pLimit(maxConcurrentTranspiles)
+      const queueTranspile = filePath => limit(() => transpileFile(filePath))
+      return Promise.all(R.map(queueTranspile, filePaths))
     })
-
-  return {
-    name: '@lerna-cola/plugin-build-flow',
-    build: () =>
-      getJsFilePaths().then(filePaths => {
-        // :: string -> Promise<void>
-        const transpileFile = filePath =>
-          new Promise(resolve => {
-            const module = path.resolve(pkg.paths.packageSrc, filePath)
-            const input = fs.readFileSync(module, 'utf8')
-            const output = flowRemoveTypes(input)
-            const outFile = path.resolve(pkg.paths.packageBuildOutput, filePath)
-            ensureParentDirectoryExists(outFile)
-            fs.writeFileSync(outFile, output.toString(), { encoding: 'utf8' })
-            fs.writeFileSync(`${outFile}.flow`, input, { encoding: 'utf8' })
-            resolve()
-          })
-
-        const limit = pLimit(maxConcurrentTranspiles)
-        const queueTranspile = filePath => limit(() => transpileFile(filePath))
-        return Promise.all(R.map(queueTranspile, filePaths))
-      }),
-    clean: () =>
-      new Promise(resolve => {
-        if (fs.pathExistsSync(pkg.paths.packageBuildOutput)) {
-          fs.removeSync(pkg.paths.packageBuildOutput)
-        }
-        resolve()
-      }),
-    deploy: () => {
-      TerminalUtils.errorPkg(pkg, '"deploy" not supported by "flow" plugin')
-      process.exit(1)
-    },
-    develop: () => {
-      TerminalUtils.errorPkg(pkg, '"develop" not supported by "flow" plugin')
-      process.exit(1)
-    },
-  }
+  },
+  clean: (pkg: Package) =>
+    new Promise(resolve => {
+      if (fs.pathExistsSync(pkg.paths.packageBuildOutput)) {
+        fs.removeSync(pkg.paths.packageBuildOutput)
+      }
+      resolve()
+    }),
+  deploy: (pkg: Package) => {
+    TerminalUtils.errorPkg(pkg, '"deploy" not supported by "flow" plugin')
+    process.exit(1)
+  },
+  develop: (pkg: Package) => {
+    TerminalUtils.errorPkg(pkg, '"develop" not supported by "flow" plugin')
+    process.exit(1)
+  },
 }
+
+module.exports = flowBuildPlugin
